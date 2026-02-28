332d5d3b98dd708d4e1960c962f0d805195d03f3
predohenr <phls2@cin.ufpe.br>
2026-02-28 10:42:05 -0300

    feature: adding s3m to experiment

---
+++ src/main/services/dataCollectors/mergeToolExecutors/s3mMergeToolExecutorDataCollector.groovy
@@ -0,0 +1,38 @@
+package services.dataCollectors.mergeToolExecutors
+
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class S3MMergeToolExecutorDataCollector extends BaseMergeToolExecutorDataCollector {
+
+    S3MMergeToolExecutorDataCollector(String extension) {
+        super(extension)
+    }
+
+    @Override
+    String getToolName() {
+        return "s3m"
+    }
+
+    @Override
+    protected List<String> getArgumentsForTool(Path scenarioDirectory, Path outputFile) {
+        Path leftFile = scenarioDirectory.resolve("left" + this.extension).toAbsolutePath()
+        Path baseFile = scenarioDirectory.resolve("base" + this.extension).toAbsolutePath()
+        Path rightFile = scenarioDirectory.resolve("right" + this.extension).toAbsolutePath()
+        
+        Path s3mJarPath = Paths.get("dependencies/s3m.jar").toAbsolutePath()
+
+        return [
+            "java", 
+            "-jar", 
+            s3mJarPath.toString(),
+            leftFile.toString(),
+            baseFile.toString(),
+            rightFile.toString(),
+            "-o", outputFile.toString(),
+            "-c", "false", 
+            "-l", "false",
+            "-hmcrd", "true"
+        ]
+    }
+}
